40bc2abe19f6f8f3a1bf1d37fc9bd17ee74d915b
Almar Klein <almar@almarklein.org>
2022-03-17 11:12:59 +0100

    Tweak readme and fix minor issue (#185)
    * tweak readme
    
    * fix that demo did not have text below menu button

--- timetagger/app/front.py
+++ timetagger/app/front.py
@@ -1052,7 +1052,7 @@ class TopWidget(Widget):
             text = "Sandbox"
         else:
             text = ""
-        text = ""
+
         sync_radius = 7
         yoffset = -6 if len(text) else 0
 
